7871c7957d539aaf6d1e183d627cdb1923253063
Leandro Rocha <leandrochaju@hotmail.com>
2025-03-17 15:02:08 -0300

    feat: Enhance file upload feature with loading modal and error handling

--- frontend/src/components/home/FileInput.tsx
+++ frontend/src/components/home/FileInput.tsx
@@ -5,12 +5,14 @@ import { useState } from "react";
 import { useTranslation } from "@/app/i18n/client";
 import { useParams } from "next/navigation";
 import api from "@/lib/axios";
+import ErrorMessage from "../feedback/ErrorMessage";
 
 export default function FileInput() {
   const params = useParams();
   const { t } = useTranslation(params?.lng as string);
   const { user } = useAuth();
   const [file, setFile] = useState<File | null>(null);
+  const [error, setError] = useState<string>('');
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -26,12 +28,25 @@ export default function FileInput() {
     const formData = new FormData();
     formData.append('file', file);
 
+    // Show loading modal
+    const modal = document.getElementById('my_modal_5') as HTMLDialogElement | null;
+    if (modal) {
+      modal.showModal();
+    }
+
     try {
       const response = await api.post('/documents/upload', formData);
       console.log(response.data);
     } catch (error) {
       console.error(error);
-      alert('An error occurred');
+
+      setError(t('home.upload.error'));
+      setTimeout(() => setError(''), 5000);
+    } finally {
+      // Hide loading modal
+      if (modal) {
+        modal.close();
+      }
     }
   }
 
@@ -49,6 +64,16 @@ export default function FileInput() {
       </fieldset>
 
       <button className="btn btn-primary" disabled={file === null} onClick={handleUpload}>{t('home.upload.button')}</button>
+
+      {/* Loading modal */}
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box flex flex-col items-center p-4 gap-4">
+          <h3 className="text-xl">{t('home.upload.uploading')}</h3>
+          <progress className="progress w-56"></progress>
+        </div>
+      </dialog>
+
+      <ErrorMessage msg={error} />
     </div>
   );
 }
