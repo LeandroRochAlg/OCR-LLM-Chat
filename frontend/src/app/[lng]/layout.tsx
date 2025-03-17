4637c24add002616410c8d71f2afe1533295a46b
Leandro Rocha <leandrochaju@hotmail.com>
2025-03-17 17:59:03 -0300

    feat: Add 'Create a new chat' link in sidebar for improved navigation

--- frontend/src/app/[lng]/layout.tsx
+++ frontend/src/app/[lng]/layout.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from '../../contexts/AuthContext';
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import { getTranslation } from '../i18n/server';
 import UserPreferences from '@/components/sidebar/UserPreferences';
+import Link from 'next/link';
 
 export async function generateStaticParams() {
   return languages.map((lng) => ({ lng }));
@@ -58,6 +59,10 @@ export default async function RootLayout({
                 <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
                 <div className='menu bg-base-200 text-base-content min-h-full w-80 p-5'>
                   <h2 className='text-xl text-bold'>OCR-LLM-Chat</h2>
+
+                  <button className='text-lg my-4 text-left btn btn-ghost'>
+                    <Link href={`/${lng}/`}>{t('sidebar.newChat')}</Link>
+                  </button>
 
                   <div className='fixed bottom-0 left-0 w-full'>
                     <UserPreferences translations={preferencesTranslations} />
